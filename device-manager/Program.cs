b2a8671c08b554839fab0a6a4eb934c7c7dae685
melihio <melihcokan@icloud.com>
2025-03-24 14:28:28 +0100

    added the logic of UI

--- device-manager/Program.cs
+++ device-manager/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("you can type help to see available commands.");
+while (true)
+{
+    var command = Console.ReadLine().Split(" ");
+
+    switch (command[0])
+    {
+        case "help":
+            Console.WriteLine(">available commands: help, add, delete");       
+            break;
+        case "add":
+            Console.WriteLine(">add");
+            break;
+        case "delete":
+            Console.WriteLine(">delete");
+            break;
+    }
+}
