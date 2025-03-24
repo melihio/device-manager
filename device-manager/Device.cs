dff2b2cce36189e3333fb4436209e9fa65939b96
melihio <melihcokan@icloud.com>
2025-03-24 15:17:37 +0100

    class PersonalComputer added

---
+++ device-manager/Device.cs
@@ -0,0 +1,8 @@
+namespace device_manager;
+
+public class Device
+{
+    public bool TurnedOn { get; set; }
+    public string id { get; set; }
+    public string name { get; set; }
+}
---
+++ device-manager/PersonalComputer.cs
@@ -0,0 +1,21 @@
+namespace device_manager;
+
+// public enum OperatingSystem
+// {
+//     Windows,
+//     MacOS,
+//     Linux,
+// }
+
+public class PersonalComputer: Device
+{
+    public string? operatingSystem { get; set; }
+
+    public PersonalComputer(string operatingSystem, bool turnedOn, string id, string name)
+    {
+        this.operatingSystem = operatingSystem;
+        this.TurnedOn = turnedOn;
+        this.id = id;
+        this.name = name;
+    }
+}
